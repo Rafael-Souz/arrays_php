3bcd1ac1888a8acfd7d8bee1fd19cf14232b376f
Rafael <rafaelso1992@gmail.com>
2021-01-29 20:47:38 -0300

    primeiro commit

---
+++ array_02.php
@@ -0,0 +1,15 @@
+<?php
+
+$carros [0] [0] = "GM"
+$carros [0] [1] = "astra"
+$carros [0] [2] = "onix"
+$carros [0] [3] = "cruze"
+
+$carros [1] [0] = "Ford"
+$carros [1] [1] = "fiesta"
+$carros [1] [2] = "fusio"
+$carros [1] [3] = "ecosporte"
+echo $carros[0][3];
+
+
+?>
---
+++ arrays_01.php
@@ -0,0 +1,7 @@
+<?php
+
+$frutas = array("laranja", "abacaxi");
+print_r($frutas)
+
+
+?>
